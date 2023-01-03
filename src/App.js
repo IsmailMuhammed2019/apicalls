04e2d0e4cc6af6737bab59f395dbc2ed26ef4a64
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-03 10:54:35 +0100

    calling api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [starWarsData, setStarWarsData] = React.useState({})
+
+  fetch("https://swapi.dev/api/people/1")
+  .then(res => res.json())
+  .then(data => setStarWarsData(data))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
     </div>
   );
 }
