c9b59fcd2585859cfb2729104c0334d11ed1f7de
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-03 16:06:23 +0100

    update useEffect data rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
-  const [starWarsData, setStarWarsData] = React.useState({})
+  const [data, setData] = useState({})
 
-  fetch("https://swapi.dev/api/people/1")
-  .then(res => res.json())
-  .then(data => setStarWarsData(data))
+  React.useEffect(() => {
+    console.log("it's working")
+      fetch('https://swapi.dev/api/people/1')
+        .then((res) => res.json())
+        .then((newdata) => setData(newdata))
+  }, [])
+
   return (
     <div className="App">
-      <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
